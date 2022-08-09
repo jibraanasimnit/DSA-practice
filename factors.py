e1c2b5386c849249023eaafbb20317d6219f62d9
Jibraan Asim <jibraan_2020bche043@nitsri.net>
2022-08-09 18:47:03 +0530

    add more code

---
+++ factors.py
@@ -0,0 +1,12 @@
+num = 12
+arr = []
+i = 1
+while i*i <= num:
+    
+    if num%i == 0:
+        arr.append(i)
+        if num//i != i:
+            arr.append(num//i)
+    i = i+1
+print(arr)
+        
