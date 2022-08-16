ca742a7ee2de38c912ec0d9abebb0b60a74f5195
Jibraan Asim <jibraan_2020bche043@nitsri.net>
2022-08-16 22:08:08 +0530

    new changes

--- dictionariesPrac.py
+++ dictionariesPrac.py
@@ -1,34 +1,49 @@
 
 
-myDict = {"name":"Edy", "age": 26,"address":"london"}
+# myDict = {"name":"Edy", "age": 26,"address":"london"}
 
-# def searchDict(dict, value):
-#     for key in dict:
-#         if dict[key] == value:
-#             print(key, value) 
-#     print('The value does not exist')
-# (searchDict(myDict, 26))
+# # def searchDict(dict, value):
+# #     for key in dict:
+# #         if dict[key] == value:
+# #             print(key, value) 
+# #     print('The value does not exist')
+# # (searchDict(myDict, 26))
 
-# myDict.pop("age")
+# # myDict.pop("age")
+# # print(myDict)
+
+# # myDict.clear()
+
+# # print(myDict)
+
+# newDict = myDict.copy()
+# # print(newDict)
+
+# for i in myDict:
+#     newNewDict = {}.fromkeys([1,2,3], myDict[i])
+#     # print(newNewDict)
+    
+# ranDict = {"name":"jibraan"}
+# (myDict.update(ranDict))
 # print(myDict)
 
-# myDict.clear()
-
-# print(myDict)
-
-newDict = myDict.copy()
-# print(newDict)
-
-for i in myDict:
-    newNewDict = {}.fromkeys([1,2,3], myDict[i])
-    # print(newNewDict)
+# if 'name' in myDict:
+#     # print(True)
     
-ranDict = {"name":"jibraan"}
-(myDict.update(ranDict))
-print(myDict)
+# for key in myDict:
+#     # print(key)
 
-if 'name' in myDict:
-    print(True)
-    
-for key in myDict:
-    print(key)
+# num_dict = {'a' : 1, 'b' : 2, 'c' : 3}
+# value = 1
+
+# for  i in num_dict:
+#     value = value*num_dict[i]
+# print(value)
+
+# keys = ['a', 'b', 'c', 'd']
+# values = [1,2,3,4]
+#                                             #ZIP KEY VALUE PAIRS IN A DICT
+# new_dict = dict(zip(keys,values)) 
+# print(new_dict)
+
+
