bae5c0fa8db9dbf6e3f79885cf6b640bf474e92a
Jibraan Asim <jibraan_2020bche043@nitsri.net>
2022-08-16 22:27:48 +0530

    new changes

--- dictionariesPrac.py
+++ dictionariesPrac.py
@@ -47,3 +47,30 @@
 # print(new_dict)
 
 
+from optparse import Values
+import string
+
+
+# string_given = "my name is name alpha alpha"
+# split_string = string_given.split()
+# d = {}
+# for i in split_string:
+#     if i not in d.keys():
+#         d[i] = 0
+#     d[i] = d[i] + 1
+# print(d)
+        
+def dict_from_list(keys, values):
+    d = {}
+    for i in range(0, len(keys)):
+        d[keys[i]] = values[i]
+    return d
+keys = [1,2,3,4,5]
+values  = [0,9,8,7,6]
+
+square_dict = {}
+def square_keys(keys):
+    for i in range(0, len(keys)):
+        square_dict[keys[i]] = keys[i]*keys[i]
+    return square_dict
+print(square_keys(keys))
