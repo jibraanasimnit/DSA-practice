dbf95172f31fbb2a868475a7c933e95dd691468c
Jibraan Asim <jibraan_2020bche043@nitsri.net>
2022-08-19 12:14:37 +0530

    more code

---
+++ oops.py
@@ -0,0 +1,9 @@
+class alpha:
+    def __init__(self, id, name, clearance):
+        self.id = id
+        self.name = name
+        self.clearance = clearance
+        
+alpha_object = alpha("ludo", "karl", 2)
+print(alpha_object.name)
+
--- singlyLinkedList.py
+++ singlyLinkedList.py
@@ -1,15 +1,58 @@
+#Linked List Insertion Method for a Printable Linked List
+
+
 class Node:
-    def __init__(self,value=None):
+    def __init__(self, value=None):
         self.value = value
         self.next = None
-class SlinkedList:
+class SLinkedList:
     def __init__(self):
         self.head = None
-        self.tail = None     
-singlyLinkedList = SlinkedList()
-node1 = Node(1)
-node2 = Node(2)
+        self.tail = None
 
-# singlyLinkedList.head = node1
-# singlyLinkedList.head.next = node2
-# singlyLinkedList.tail = node2
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            node = node.next
+    def appendLL(self, value, location):
+        newNode = Node(value)
+        if self.head is None:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            if location == 0:
+                newNode.next = self.head
+                self.head = newNode
+            elif location == 1:
+                newNode.next = None
+                self.tail.next = newNode
+                self.tail = newNode
+            else:
+                tempNode = self.head
+                index = 0
+                while index < location - 1:
+                    tempNode = tempNode.next
+                    index = index + 1
+                nextNode = tempNode.next
+                tempNode = newNode
+                newNode.next = nextNode
+                
+
+
+
+
+linkedList = SLinkedList()
+linkedList.appendLL(1,1)
+linkedList.appendLL(2,1)
+linkedList.appendLL(3,1)
+linkedList.appendLL(4,1)
+print([node.value for node in linkedList])
+
+
+    
+
+
+
+            
+    
