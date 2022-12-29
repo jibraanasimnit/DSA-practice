85588618ffea59e4bcbe0df4c6ddb14a10ce470f
Jibraan Asim <jibraan_2020bche043@nitsri.net>
2022-12-29 19:02:59 +0530

    coding in assam

---
+++ Q1_stack.py
@@ -0,0 +1,54 @@
+#using a a single list to implement 3 stacks
+
+class MultiStacks:
+    def __init__(self, stacksize):
+        self.numberstacks = 3
+        self.custList = [0] * (stacksize*self.numberstacks)
+        self.sizes = [0] * self.numberstacks
+        self.stacksize = stacksize
+
+    def isFull(self, stacknum):
+        if self.sizes[stacknum] == self.stacksize:
+            return True
+        else:
+            return False
+    def isEmpty(self, stacknum):
+        if self.sizes[stacknum] == 0:
+            return True
+        else:
+            return False
+    
+    def indexOfTop(self, stacknum):
+        offset = stacknum * self.stacksize
+        return offset + self.sizes[stacknum] - 1
+
+    def push(self, item, stacknum):
+        if self.isFull(stacknum):
+            return "stack is full"
+        else:
+            self.sizes[stacknum] += 1
+            self.custList[self.indexOfTop(stacknum)] = item
+    def pop(self, stacknum):
+        if self.isEmpty(stacknum):
+            return "stack is empty"
+        else:
+            value = self.custList[self.indexOfTop(stacknum)]
+            self.custList[self.indexOfTop(stacknum)] = 0
+            self.sizes[stacknum] -= 1
+            return value
+    def peek(self, stacknum):
+        if self.isEmpty(stacknum):
+            return "stack is empty"
+        else:
+            value = self.custList[self.indexOfTop(stacknum)]
+            return value
+    
+customStack = MultiStacks(6)
+print(customStack.isFull(0))
+print(customStack.isEmpty(1))
+customStack.push(1,0)
+customStack.push(2,0)
+customStack.push(3,2)
+customStack.pop(2)
+print(customStack.peek(2))
+     
